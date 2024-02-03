94c61905fa18dc047130d14fabbfc12bfe6d4f29
cementix <bodakondrackij@gmail.com>
2024-02-03 08:11:43 +0200

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import TopBar from "@/components/shared/TopBar";
+import LeftSideBar from "@/components/shared/LeftSideBar";
+import RightSideBar from "@/components/shared/RightSideBar";
+import BottomBar from "@/components/shared/BottomBar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Threads",
+  description: "A Next.js 14 Meta Threads Application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <TopBar />
+
+          <main>
+            <LeftSideBar />
+
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+
+            <RightSideBar />
+          </main>
+
+          <BottomBar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
